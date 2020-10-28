b42a193a00ba587a6e4fa5f5ee8dc0773a25d08d
Akash Dubey <akashdubey826@gmail.com>
2020-10-29 01:21:53 +0530

    Update getting_started.py

--- getting_started.py
+++ getting_started.py
@@ -1,7 +1,10 @@
 '''
 Sparkcontext is the entry point for spark environment. For every sparkapp you need to create the sparkcontext object.
-Sparkconf is the class which gives you the various option to provide configuration parameters.
+Sparkconf is the class which gives you the various option to provide configuration parameters. The spark configuration 
+is passed to spark context.You can also set different application configuration in sparkconf and pass to sparkcontex
 '''
+import pyspark
+from pyspark import SparkContext, SparkConf
 
-conf= SparkConf().setAppName('bm25-hyperparameter-tuning')
+conf= SparkConf().setAppName('hello-test')
 sc = SparkContext(conf=conf)
